2191cd0dffb9966337ba3f1ebe6a00bc7365f197
Belinschi Dima <belinski.d@gmail.com>
2016-02-22 18:16:11 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testhero::Application.config.secret_token = '42fbd006c597419cb1030b577dcd82dad6fc65064d807e3d761c29670e703472fb67c3f003764a8bca0f85adce0522267fd8024646308f93a1c83d363d757964'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Testhero::Application.config.session_store :cookie_store, key: '_testhero_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Testhero::Application.config.session_store :active_record_store
