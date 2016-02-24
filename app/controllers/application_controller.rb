de5239680c9cad8219c4677df074bcb1b88e1ca3
zelcam <zelcam@gmail.com>
2016-02-25 00:17:37 +0700

    user, admin, roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery with: :exception
+
+  def authenticate_admin!
+    authenticate_user!
+
+    unless current_user.admin?
+      redirect_to root_path, alert: "You are not allowed to do this!"
+    end
+  end
 end
